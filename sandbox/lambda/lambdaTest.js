5060717f36f7a558f7fb6a4f9e9fcd66b09d04d9
Florian Hug <florian.hug.98@gmail.com>
2026-01-31 19:11:17 +0100

    plotter

--- sandbox/lambda/lambdaTest.js
+++ sandbox/lambda/lambdaTest.js
@@ -1,58 +1,58 @@
-const ok = [];
+const lambdaOk = [];
 
 //id
-ok.push(id(1) === 1);
-ok.push(id(id) === id);
+lambdaOk.push(id(1) === 1);
+lambdaOk.push(id(id) === id);
 
 //konst
-ok.push(konst(42)(0) === 42);
-ok.push(konst(42)(1) === 42);
-ok.push(konst(42)(undefined) === 42);
-ok.push(konst(42)(null) === 42);
-ok.push(konst(42)(NaN) === 42);
+lambdaOk.push(konst(42)(0) === 42);
+lambdaOk.push(konst(42)(1) === 42);
+lambdaOk.push(konst(42)(undefined) === 42);
+lambdaOk.push(konst(42)(null) === 42);
+lambdaOk.push(konst(42)(NaN) === 42);
 
 //kite
-ok.push(snd(null)(400) === 400);
+lambdaOk.push(snd(null)(400) === 400);
 
 //true false
-ok.push(T(1)(0) === 1);
-ok.push(F(0)(1) === 1);
+lambdaOk.push(T(1)(0) === 1);
+lambdaOk.push(F(0)(1) === 1);
 
 //and
-ok.push(and(F)(F) === F);
-ok.push(and(F)(T) === F);
-ok.push(and(T)(F) === F);
-ok.push(and(T)(T) === T);
+lambdaOk.push(and(F)(F) === F);
+lambdaOk.push(and(F)(T) === F);
+lambdaOk.push(and(T)(F) === F);
+lambdaOk.push(and(T)(T) === T);
 
 //or
-ok.push(or(F)(F) === F);
-ok.push(or(T)(F) === T);
-ok.push(or(F)(T) === T);
-ok.push(or(T)(T) === T);
+lambdaOk.push(or(F)(F) === F);
+lambdaOk.push(or(T)(F) === T);
+lambdaOk.push(or(F)(T) === T);
+lambdaOk.push(or(T)(T) === T);
 
 //xor
-ok.push(xor(F)(F) === F);
-ok.push(xor(T)(F) === T);
-ok.push(xor(F)(T) === T);
-ok.push(xor(T)(T) === F);
+lambdaOk.push(xor(F)(F) === F);
+lambdaOk.push(xor(T)(F) === T);
+lambdaOk.push(xor(F)(T) === T);
+lambdaOk.push(xor(T)(T) === F);
 
 //flip
-ok.push(flip(konst)(1)(0) === konst(0)(1));
+lambdaOk.push(flip(konst)(1)(0) === konst(0)(1));
 
 //not
-ok.push(not(T) === F);
-ok.push(not(F) === T);
+lambdaOk.push(not(T) === F);
+lambdaOk.push(not(F) === T);
 
 //beq
-ok.push(beq(F)(F) === T);
-ok.push(beq(T)(F) === F);
-ok.push(beq(F)(T) === F);
-ok.push(beq(T)(T) === T);
+lambdaOk.push(beq(F)(F) === T);
+lambdaOk.push(beq(T)(F) === F);
+lambdaOk.push(beq(F)(T) === F);
+lambdaOk.push(beq(T)(T) === T);
 
 //pair
 const fhug = Pair("Florian")("Hug"); // Immutable -> wegen closure scope
-ok.push(fhug(firstname) === "Florian");
-ok.push(fhug(lastname) === "Hug");
+lambdaOk.push(fhug(firstname) === "Florian");
+lambdaOk.push(fhug(lastname) === "Hug");
 
 //either
 const saveDiv = num => divisor =>
@@ -77,10 +77,10 @@ saveDiv(1)(0)
 
 
 
-
+document.writeln("lambda:<br>")
 const allTestsOk = () => {
-    for (let i = 0; i < ok.length; i++) { // not nice, yet. Needs improvement
-        if (false === ok[i]) {
+    for (let i = 0; i < lambdaOk.length; i++) { // not nice, yet. Needs improvement
+        if (false === lambdaOk[i]) {
             return false;
         }
     }
@@ -88,11 +88,11 @@ const allTestsOk = () => {
 };
 
 if (allTestsOk()) {
-    document.writeln("All " + ok.length + " tests ok.");
+    document.writeln("All " + lambdaOk.length + " tests ok.<br><br>");
 } else {
     document.writeln("Not all tests ok! Details:");
-    for (let i = 0; i < ok.length; i++) {
-        if (ok[i]) {
+    for (let i = 0; i < lambdaOk.length; i++) {
+        if (lambdaOk[i]) {
             document.writeln("Test " + i + " ok");
         } else {
             document.writeln("Test " + i + " failed");
---
+++ sandbox/plotter/plotter.js
@@ -0,0 +1,48 @@
+const minX =  0;
+const maxX =  6;
+const minY = -1;
+const maxY =  1;
+
+function start() {
+    const userFunction = document.getElementById('user_function');
+    const canvas       = document.getElementById('canvas');
+
+    // userFunction wird nur einmal geparst!!!
+    const calculate    = ()     => Function("x", "return "+ userFunction.value);
+
+    // underscore -> when value exlizit ignoriert werden soll
+    const handler      = _event => display(canvas, calculate());
+
+    handler();
+    userFunction.onchange = handler;
+}
+
+function display(canvas, f) {
+    // clear
+    const context     = canvas.getContext("2d");
+    context.fillStyle = "papayawhip";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // draw the function plot
+    const normx = normalizeX(canvas.width);
+    const normy = normalizeY(canvas.height);
+
+    context.fillStyle = "black";
+    context.beginPath();
+    context.moveTo(normx(minX), normy(f(minX)));
+
+    const stride = (maxX - minX) / 100; // 100 Stützstellen
+    for (let x = minX; x <= maxX; x += stride) {
+        context.lineTo(normx(x), normy(f(x)));
+        context.stroke();
+    }
+}
+
+const normalizeY = height => y => {
+    const scaleFactor = height / (maxY - minY);
+    return height - (y - minY) * scaleFactor;
+};
+
+const normalizeX = width => x => {
+    const scaleFactor = width / (maxX - minX);
+    return ( x - minX) * scaleFactor;
+};
